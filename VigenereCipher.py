718ca9fbc441de8c39b8167d7d717c885b213b37
saiAkulaPropertiesGit <saiakulaproperties@gmail.com>
2021-03-18 13:13:45 +0530

    converted findSize and VingenerCiper into pep8 standards

--- VigenereCipher.py
+++ VigenereCipher.py
@@ -1,96 +1,139 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+
+"""
+    Blaise de Vigenère (1523–1596) mathematician, developed encryption scheme,
+    VigenereCipher algorithm  is implemented based on his work, with a utility
+    of relative strength index for encryption and decryption.
+
+    VERSION : 1.0
+    LICENSE : GNU GPLv3
+    STYLE   : PEP 8
+    AUTHOR  : AKULA.S.S.S.R.Krishna
+    Date    : 05/11/2020
+
+    PURPOSE : To encrypt and decrypt text based files
+    INPUT   : python3 VingenerCipher -i sample_file.txt -e "sample password"
+    OUTPUT  : sample_file.txt will be replaced with encrypted data.
+"""
+
+
 import os
 import argparse
+
+
 class VigenereCipher(object):
-    def __init__(self,key):
+    def __init__(self, key):
         print('Vigenere Cipher Encription')
-        self.key=key
-    
-    def encode(self, text):    
-        key=self.key
-        ans=''
-        for I,i in enumerate(text):
-            if(ord('!')<=ord(i)<=ord('~')):
-                I=I%len(key)
-                if( ord(i)+ord(key[I])-ord('!') > ord('~') ):
-                    ans+=(chr(ord('!')+(ord(i)+ord(key[I])-ord('!'))%ord('~')-1))
+        self.key = key
+
+    def encode(self, text):     # Based on password every character
+        key = self.key              # will be encrypted with different bias
+        ans = ''
+        for index, i in enumerate(text):
+            if(ord('!') <= ord(i) <= ord('~')):
+                index %= len(key)
+                if(ord(i) + ord(key[index]) - ord('!') > ord('~')):
+                    ans += (chr(ord('!') + (ord(i) + ord(key[index])
+                            - ord('!')) % ord('~') - 1))
                 else:
-                    ans+=(chr(ord(i)+ord(key[I])-ord('!')))
+                    ans += (chr(ord(i) + ord(key[index]) - ord('!')))
             else:
-                ans+=i
-        return ans
-    
-    def decode(self, text):     
-        key=self.key
-        ans=''
-        for I,i in enumerate(text):
-            if(ord('!')<=ord(i)<=ord('~')):
-                I=I%len(key)
-                if( (ord('!')+ord(i)-ord(key[I])) < ord('!')):
-                    ans+=(chr( ord('~')+(ord(i)-ord(key[I]))+1))
-                else:
-                    ans+=(chr( ord('!')+ord(i)-ord(key[I])))
-            else:
-                ans+=i
+                ans += i
         return ans
 
+    def decode(self, text):     # Based on password every character
+        key = self.key              # will be decrypted with different bias
+        ans = ''
+        for index, i in enumerate(text):
+            if(ord('!') <= ord(i) <= ord('~')):
+                index %= len(key)
+                if((ord('!') + ord(i) - ord(key[index])) < ord('!')):
+                    ans += (chr(ord('~') + (ord(i) - ord(key[index])) + 1))
+                else:
+                    ans += (chr(ord('!') + ord(i) - ord(key[index])))
+            else:
+                ans += i
+        return ans
+
+
 def read_from_file(file_name):
-    f=open(file_name,'r')
-    data=f.read()
+    f = open(file_name, 'r')
+    data = f.read()
     f.close()
     return data
 
-def write_to_file(file_name,data):
-    f=open(file_name,'w')
-    data=f.write(data)
+
+def write_to_file(file_name, data):
+    f = open(file_name, 'w')
+    data = f.write(data)
     f.close()
 
-def encode_from_file(file_name,obj):
-    data=read_from_file(file_name)
-    for _ in range(args.strength):
-        data= obj.encode(data)
-    write_to_file(file_name,data)
-    print('encode file -> '+file_name)
 
-def decode_from_file(file_name,obj):
-    data=read_from_file(file_name)
+def encode_from_file(file_name, obj):
+    data = read_from_file(file_name)
     for _ in range(args.strength):
-        data= obj.decode(data)
-    write_to_file(file_name,data)    
-    print('decode file -> '+file_name)
+        data = obj.encode(data)
+    write_to_file(file_name, data)      # Replaces file with encrypted data
+    print('encode file -> ' + file_name)
 
-def encription_form_path(PATH,obj):
-        try:
-                for path in os.listdir(PATH):
-                        encription_form_path(PATH+'/'+path,obj)
-        except:
-                if(args.encode):
-                        encode_from_file(PATH,obj)
-                elif(args.decode):   
-                        decode_from_file(PATH,obj)
-    
-parser = argparse.ArgumentParser(description='Description of your program')
-parser.add_argument('-i','--input_file', help='input file name', required=False)
-parser.add_argument('-e','--encode', help='encode password', required=False)
-parser.add_argument('-d','--decode', help='decode password', required=False)
-parser.add_argument('-f','--folder',help='folder name',required=False)
-parser.add_argument('-s','--strength', help='encription strength', type=int, default=1, required=False)
-args =(parser.parse_args())
+
+def decode_from_file(file_name, obj):
+    data = read_from_file(file_name)
+    for _ in range(args.strength):
+        data = obj.decode(data)
+    write_to_file(file_name, data)      # Replaces file with decrypted data
+    print('decode file -> ' + file_name)
+
+
+def encription_form_path(PATH, obj):    # Recursive function (MT-safe)
+    try:
+        for path in os.listdir(PATH):
+            encription_form_path(PATH + '/' + path, obj)
+    except(OSError):
+        if(args.encode):
+            encode_from_file(PATH, obj)
+        elif(args.decode):
+            decode_from_file(PATH, obj)
+
+
+"""
+    input can be either -i file / -f folder,
+    encode -e, decode -d for encryption and decryption respectively,
+    strength -s indicates number of times to be encrypted / decrypted.
+
+"""
+
+
+parser = argparse.ArgumentParser('Description of your program')
+parser.add_argument('-i', '--input_file',
+                    help='input file name', required=False)
+parser.add_argument('-e', '--encode',
+                    help='encode password', required=False)
+parser.add_argument('-d', '--decode',
+                    help='decode password', required=False)
+parser.add_argument('-f', '--folder',
+                    help='folder name', required=False)
+parser.add_argument('-s', '--strength',
+                    help='encription strength', type=int,
+                    default=1, required=False)
+args = (parser.parse_args())
 
 if(args.input_file):
-    PATH=args.input_file
+    PATH = args.input_file
 elif(args.folder):
-    PATH=args.folder
+    PATH = args.folder
 else:
     exit('Need --input_file or --folder\nUse -h for help')
 
 if(args.encode):
-    pswd=args.encode
+    pswd = args.encode
 elif(args.decode):
-    pswd=args.decode
+    pswd = args.decode
 else:
     exit('Need --encode or --decode\nUse -h for help')
 
 
-obj=VigenereCipher(pswd)
-encription_form_path(PATH,obj)
-
+obj = VigenereCipher(pswd)
+encription_form_path(PATH, obj)
--- findSize.py
+++ findSize.py
@@ -1,47 +1,61 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 
-# In[36]:
+"""
+    To find the size of a directory in human  readable format,
+    as a utility gives each file size.
 
+    VERSION : 1.0
+    LICENSE : GNU GPLv3
+    STYLE   : PEP 8
+    AUTHOR  : AKULA.S.S.S.R.Krishna
+    DATE    : 15/01/2021
+    
+    PURPOSE : To find size of a directory, along with all the hidden file sizes
+    INPUT   : python3 findSize.py -i ./sample_folder -e 1
+    OUTPUT  : List of each file with their respective size
+"""
 
 import os
 import argparse
 
-parser = argparse.ArgumentParser(description='Description of your program')
-parser.add_argument('-i','--input_dir_path', help='input file name', required=True)
-parser.add_argument('-e','--every_file', help='gives size of every file', required=False)
-args =(parser.parse_args())
+parser = argparse.ArgumentParser('Description of your program')
+parser.add_argument('-i', '--input_dir_path',
+                    help='input file name', required=True)
+parser.add_argument('-e', '--every_file',
+                    help='gives size of every file', required=False)
+args = parser.parse_args()
+
 
 class findSize(object):
-   
     def __init__(self,):
-        self.size=0
+        self.size = 0
 
-    #To get size in human redable format 
-    def get_size_hr(self,size_bytes):
-        if(size_bytes<1000):
-            return size_bytes,'Bytes'
-        elif(size_bytes<(10**6)):
-            return size_bytes/(10**3),'KB'
-        elif(size_bytes<(10**9)):
-            return size_bytes/(10**6),'MB'
+    def get_size_hr(self, size_bytes):   # To get size in human readable format
+        if(size_bytes < 1000):
+            return size_bytes, 'Bytes'
+        elif(size_bytes < (10**6)):
+            return size_bytes / (10**3), 'KB'
+        elif(size_bytes < (10**9)):
+            return size_bytes / (10**6), 'MB'
         else:
-            return size_bytes/(10**9),'GB'
+            return size_bytes / (10**9), 'GB'
 
-    def get_dir_size(self, path):
+    def get_dir_size(self, path):   # Recursive function (MT-safe)
         for name in sorted(os.listdir(path)):
             try:
-                self.get_dir_size(path+'/'+name)
-            except:
-                file_size=int((os.stat(path+'/'+name).st_size))
+                self.get_dir_size(path + '/' + name)
+            except(OSError):
+                file_size = int((os.stat(path + '/' + name).st_size))
                 if(args.every_file):
-                    print(name," - ",self.get_size_hr(file_size))
-                self.size+=file_size
+                    print(name, " - ", self.get_size_hr(file_size))
+                self.size += file_size
         return self.size
 
-    def get_size(self,):
-        print('Total size = ',self.get_size_hr(self.get_dir_size(args.input_dir_path)))
+    def get_size(self, ):
+        print('Total size = ', self.get_size_hr(
+             self.get_dir_size(args.input_dir_path)))
 
-obj=findSize()
+
+obj = findSize()
 obj.get_size()
-
